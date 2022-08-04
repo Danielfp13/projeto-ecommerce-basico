c418cc7b6d6ea6b99df6f1583d51a6b4e9a9998c
Daniel Pereira <daniel.si@outlook.com.br>
2022-08-04 17:20:02 -0300

    front e-commerce basico em angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { BooksService } from './components/book-cart/product-list/product-list.service';
+import { NavComponent } from './components/shared/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { BookCartComponent } from './components/book-cart/book-cart.component';
+import { FiltersComponent } from './components/book-cart/filters/filters.component';
+import { ProductListComponent } from './components/book-cart/product-list/product-list.component';
+import { ProductItemComponent } from './components/book-cart/product-list/product-item/product-item.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    HeaderComponent,
+    BookCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
